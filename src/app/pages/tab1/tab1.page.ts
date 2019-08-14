fb24337784faf77c1ea1bec18da7f182d28f0552
israel <israel.bejarano.a@gmail.com>
2019-08-14 21:35:03 +0200

    solved warning

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -6,7 +6,7 @@ import { NoticiasService } from '../../services/noticias.service';
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page implements OnInit{
+export class Tab1Page implements OnInit {
 
   constructor(private noticiasService: NoticiasService) {}
 
