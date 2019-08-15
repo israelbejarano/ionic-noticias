1742355e839b51b43deb1ebd5c2b63d68c862084
israel <israel.bejarano.a@gmail.com>
2019-08-15 11:06:32 +0200

    interfaces de la api

--- src/app/services/noticias.service.ts
+++ src/app/services/noticias.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RespuestaTopHeadLines } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ export class NoticiasService {
   constructor(private http: HttpClient) { }
 
   getTopHeadLines() {
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e26c007e97b244a0b7e90c1c2d64e05b`);
+    return this.http.get<RespuestaTopHeadLines>
+      (`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=e26c007e97b244a0b7e90c1c2d64e05b`);
   }
 }
