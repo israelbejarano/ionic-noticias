4ad0c20357859b20ef604f4660b522ec4ad42038
israel <israel.bejarano.a@gmail.com>
2019-08-16 15:24:12 +0200

    función compartir en el movil habilitada

--- src/app/components/noticia/noticia.component.ts
+++ src/app/components/noticia/noticia.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Article } from '../../interfaces/interfaces';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 import { SocialSharing } from '@ionic-native/social-sharing/ngx';
-import { ActionSheetController } from '@ionic/angular';
+import { ActionSheetController, Platform } from '@ionic/angular';
 import { DataLocalService } from '../../services/data-local.service';
 
 @Component({
@@ -17,7 +17,8 @@ export class NoticiaComponent implements OnInit {
   @Input() enFavoritos;
 
   constructor(private iab: InAppBrowser, private socialSharing: SocialSharing,
-              private actionSheetCtrl: ActionSheetController, private dataLocalService: DataLocalService) { }
+              private actionSheetCtrl: ActionSheetController, private dataLocalService: DataLocalService,
+              private platform: Platform) { }
 
   ngOnInit() {}
 
@@ -55,7 +56,7 @@ export class NoticiaComponent implements OnInit {
         cssClass: 'action-dark',
         handler: () => {
           console.log('Share clicked');
-          this.socialSharing.share(this.noticia.title, this.noticia.source.name, '', this.noticia.url);
+          this.compartirNoticia();
         }
       },
         guardarBorrarBtn,
@@ -70,6 +71,24 @@ export class NoticiaComponent implements OnInit {
       }]
     });
     await actionSheet.present();
+  }
+
+  compartirNoticia() {
+    if (this.platform.is('cordova')) {
+      this.socialSharing.share(this.noticia.title, this.noticia.source.name, '', this.noticia.url);
+    } else {
+      if (navigator.share) {
+        navigator.share({
+            title: this.noticia.title,
+            text: this.noticia.description,
+            url: this.noticia.url,
+        })
+          .then(() => console.log('Successful share'))
+          .catch((error) => console.log('Error sharing', error));
+      } else {
+        console.log('función no soportada');
+      }
+    }
   }
 
 }
